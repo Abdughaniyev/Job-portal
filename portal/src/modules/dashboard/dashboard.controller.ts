a2ecc8507158f7429303c0c6fd82786a66309004
FakhrAl-din <hitpitgym04@gmail.com>
2025-06-28 16:28:29 +0300

    Added Toker related settings

--- portal/src/modules/dashboard/dashboard.controller.ts
+++ portal/src/modules/dashboard/dashboard.controller.ts
@@ -3,9 +3,11 @@ import { JwtAuthGuard } from "../users/jwt/auth-guard";
 import { RolesGuard } from "../users/roles/roles.guard";
 import { Roles } from "../users/roles/roles.decorator";
 import { DashboardService } from "./dashboard.service";
+import { ApiBearerAuth } from "@nestjs/swagger";
 
 @Controller('dashboard')
 @UseGuards(JwtAuthGuard, RolesGuard)
+@ApiBearerAuth('access-token')
 @Roles('admin')
 export class DashboardController {
     constructor(private readonly dashboardService: DashboardService) { }
