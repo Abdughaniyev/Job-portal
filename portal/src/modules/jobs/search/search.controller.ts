3a96b6b1538edea2fa27d54bb6a38d0a4377fc5c
FakhrAl-din <hitpitgym04@gmail.com>
2025-07-04 22:32:31 +0300

    Added Bearer Auth

--- portal/src/modules/jobs/search/search.controller.ts
+++ portal/src/modules/jobs/search/search.controller.ts
@@ -2,7 +2,9 @@ import { Controller, Get, Query, UseGuards } from "@nestjs/common";
 import { JwtAuthGuard } from "../../users/jwt/auth-guard";
 import { Roles } from "../../users/roles/roles.decorator";
 import { SearchService } from "./search.service";
+import { ApiBearerAuth } from "@nestjs/swagger";
 
+@ApiBearerAuth('access-token')
 @Controller('search')
 export class SearchController {
     constructor(private readonly searchService: SearchService) { }
