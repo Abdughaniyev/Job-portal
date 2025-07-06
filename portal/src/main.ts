58c047d6e385546ad8e33894dc49dc4a2bcbd2f3
FakhrAl-din <hitpitgym04@gmail.com>
2025-07-06 20:08:20 +0300

    Added correct host

--- portal/src/main.ts
+++ portal/src/main.ts
@@ -14,15 +14,15 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   // Allow all origins in production, only localhost in dev
- app.enableCors({
+  app.enableCors({
     origin:
       process.env.NODE_ENV === 'production'
         ? process.env.FRONTEND_URL
-        : 'http://localhost:5176',
+        : 'http://localhost:5173',
     credentials: true,
   });
 
-  
+
   app.useBodyParser('json');
   app.use(helmet());
   // Global prefix for all routes (e.g., /api/v1/users)
