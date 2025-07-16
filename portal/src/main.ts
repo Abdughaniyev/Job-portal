2c7e568f59170b9c1846bea153085cbbc4ffb1cf
FakhrAl-din <hitpitgym04@gmail.com>
2025-07-16 03:26:58 +0300

    Added CORS changes

--- portal/src/main.ts
+++ portal/src/main.ts
@@ -15,11 +15,10 @@ async function bootstrap() {
 
   // Allow all origins in production, only localhost in dev
   app.enableCors({
-    origin:
-      process.env.NODE_ENV === 'production'
-        ? process.env.FRONTEND_URL
-        : 'http://localhost:5173',
-    credentials: true,
+    origin: 'http://localhost:5173'
+    // process.env.NODE_ENV === 'production'
+    // ? process.env.FRONTEND_URL
+    // : 'http://localhost:5173',
   });
 
 
