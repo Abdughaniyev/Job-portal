47c12390cea06184120dad29a4fd3e2ee9591762
FakhrAl-din <hitpitgym04@gmail.com>
2025-09-19 14:26:50 +0300

    SSPL

--- portal/src/main.ts
+++ portal/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: 'https://localhost:5173',
     credentials: true,
   });
 
