87536fda9df5d715a0daaf385c80dd159bafa4e7
Fakhriddin <hitpitgym04@gmail.com>
2025-11-23 12:02:15 -0800

    Google login

--- portal/src/modules/users/google/google.strategy.ts
+++ portal/src/modules/users/google/google.strategy.ts
@@ -3,30 +3,31 @@ import { Strategy, VerifyCallback } from 'passport-google-oauth20';
 import { Injectable } from '@nestjs/common';
 import { config } from 'src/common/config/config';
 
-
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+  constructor() {
+    super({
+      clientID: config.clientID,
+      clientSecret: config.clientSecret,
+      callbackURL: config.callbackURL,
+      // callbackURL: config.callbackURL, // for local host
+      scope: ['email', 'profile'],
+      prompt: 'select_account',
+    });
+  }
 
-    constructor() {
-        super({
-
-            clientID: config.clientID,
-            clientSecret: config.clientSecret,
-            callbackURL: config.callbackURL,
-            // callbackURL: config.callbackURL, // for local host
-            scope: ['email', 'profile'],
-        });
-    }
-
-    async validate(accessToken: string, refreshToken: string, profile: any, done: VerifyCallback): Promise<any> {
-        const { name, emails } = profile;
-        const user = {
-            fullName: name.givenName + ' ' + name.familyName,
-            email: emails[0].value,
-            provider: 'google',
-        };
-        done(null, user);
-    }
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: any,
+    done: VerifyCallback,
+  ): Promise<any> {
+    const { name, emails } = profile;
+    const user = {
+      fullName: name.givenName + ' ' + name.familyName,
+      email: emails[0].value,
+      provider: 'google',
+    };
+    done(null, user);
+  }
 }
-
-
