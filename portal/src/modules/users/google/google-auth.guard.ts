4b9dbd69b11c069b996e9ccada5bd5fba79f445b
Fakhriddin <hitpitgym04@gmail.com>
2025-11-23 12:09:15 -0800

    Login fix

--- portal/src/modules/users/google/google-auth.guard.ts
+++ portal/src/modules/users/google/google-auth.guard.ts
@@ -2,4 +2,10 @@ import { AuthGuard } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class GoogleAuthGuard extends AuthGuard('google') { }
+export class GoogleAuthGuard extends AuthGuard('google') {
+  constructor() {
+    super({
+      prompt: 'select_account',
+    });
+  }
+}
--- portal/src/modules/users/google/google.strategy.ts
+++ portal/src/modules/users/google/google.strategy.ts
@@ -3,31 +3,30 @@ import { Strategy, VerifyCallback } from 'passport-google-oauth20';
 import { Injectable } from '@nestjs/common';
 import { config } from 'src/common/config/config';
 
+
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
-  constructor() {
-    super({
-      clientID: config.clientID,
-      clientSecret: config.clientSecret,
-      callbackURL: config.callbackURL,
-      // callbackURL: config.callbackURL, // for local host
-      scope: ['email', 'profile'],
-      prompt: 'select_account',
-    });
-  }
 
-  async validate(
-    accessToken: string,
-    refreshToken: string,
-    profile: any,
-    done: VerifyCallback,
-  ): Promise<any> {
-    const { name, emails } = profile;
-    const user = {
-      fullName: name.givenName + ' ' + name.familyName,
-      email: emails[0].value,
-      provider: 'google',
-    };
-    done(null, user);
-  }
+    constructor() {
+        super({
+
+            clientID: config.clientID,
+            clientSecret: config.clientSecret,
+            callbackURL: config.callbackURL,
+            // callbackURL: config.callbackURL, // for local host
+            scope: ['email', 'profile'],
+        });
+    }
+
+    async validate(accessToken: string, refreshToken: string, profile: any, done: VerifyCallback): Promise<any> {
+        const { name, emails } = profile;
+        const user = {
+            fullName: name.givenName + ' ' + name.familyName,
+            email: emails[0].value,
+            provider: 'google',
+        };
+        done(null, user);
+    }
 }
+
+
